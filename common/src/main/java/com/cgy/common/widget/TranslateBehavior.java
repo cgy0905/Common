f06c84ded746ab5440898d9db32ff4459f2a0cee
陈国玉 <chenguoyu@heshidai.com>
2018-07-25 16:08:30 +0800

    自定义一个behavior

---
+++ common/src/main/java/com/cgy/common/widget/TranslateBehavior.java
@@ -0,0 +1,10 @@
+package com.cgy.common.widget;
+
+import android.support.design.widget.FloatingActionButton;
+
+/**
+ * Created by cgy
+ * 2018/7/25  14:14
+ */
+public class TranslateBehavior extends FloatingActionButton.Behavior {
+}
